dd08ee978e93413273acbd08338096204530c775
Asto03 <stephan.port@stud.hcw.ac.at>
2026-03-24 02:21:42 +0100

    Number 1, 2 and 3 done Originally in branche exercise-1

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const {parse} = require("nodemon/lib/cli");
 const app = express()
 
 // Serve static content in directory 'files'
@@ -8,10 +9,76 @@ app.use(express.static(path.join(__dirname, 'files')));
 // Configure a 'get' endpoint for data..
 app.get('/movies', function (req, res) {
   // Part 1: Remove the next line and replace with your code
-  res.send('!dlrow olleH')
+  const movies =[
+    {
+      "Title": "Spirited Away",
+      "Released": "2003-03-28",
+      "Runtime": 124,
+      "Genres": ["Animation", "Adventure", "Family"],
+      "Directors": ["Hayao Miyazaki"],
+      "Writers": ["Hayao Miyazaki"],
+      "Actors": ["Miyu Irino", "Rumi Hiiragi", "Mari Natsuki"],
+      "Plot": "During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches and spirits, and where humans are changed into beasts.",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BNTEyNmEwOWUtYzkyOC00ZTQ4LTllZmUtMjk0Y2YwOGUzYjRiXkEyXkFqcGc@._V1_SX300.jpg",
+      "Metascore": 96,
+      "imdbRating": 8.6
+    },
+    {
+      "Title": "The Truman Show",
+      "Released": "1998-06-05",
+      "Runtime": 103,
+      "Genres": ["Comedy", "Drama"],
+      "Directors": ["Peter Weir"],
+      "Writers": ["Andrew Niccol"],
+      "Actors": ["Jim Carrey", "Ed Harris", "Laura Linney"],
+      "Plot": "An insurance salesman begins to suspect that his whole life is actually some sort of reality TV show.",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BNzA3ZjZlNzYtMTdjMy00NjMzLTk5ZGYtMTkyYzNiOGM1YmM3XkEyXkFqcGc@._V1_SX300.jpg",
+      "Metascore": 90,
+      "imdbRating": 8.2
+    },
+    {
+      "Title": "Dragon Ball Super: Broly",
+      "Released": "2019-01-16",
+      "Runtime": 100,
+      "Genres": ["Animation", "Action", "Adventure"],
+      "Directors": ["Tatsuya Nagamine"],
+      "Writers": ["Akira Toriyama"],
+      "Actors": ["Masako Nozawa", "Aya Hisakawa", "Ryô Horikawa"],
+      "Plot": "Goku and Vegeta encounter Broly, a Saiyan warrior unlike any fighter they've faced before.",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMTA5MTc1M2EtZWQ2Ni00ZmU2LTg3MzQtOTliMjE4OGM0ZWFiXkEyXkFqcGc@._V1_SX300.jpg",
+      "Metascore": 59,
+      "imdbRating": 7.7
+    },
+    {
+      "Title": "Princess Mononoke",
+      "Released": "1997-07-12",
+      "Runtime": 133,
+      "Genres": ["Animation", "Adventure", "Fantasy"],
+      "Directors": ["Hayao Miyazaki"],
+      "Writers": ["Hayao Miyazaki"],
+      "Actors": ["Yôji Matsuda", "Yuriko Ishida", "Yûko Tanaka"],
+      "Plot": "While seeking to cure himself of a curse, young warrior Ashitaka stumbles into a conflict between the people of Iron Town and Princess Mononoke, a girl raised by wolves, who will stop at nothing to prevent the destruction of her h...",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BNjAzODViNGEtMjBkYi00N2U5LWJjNjAtY2U0MGJhZTEwOTU0XkEyXkFqcGc@._V1_SX300.jpg",
+      "Metascore": 78,
+      "imdbRating": 8.3
+    },
+    {
+      "Title":"American Psycho",
+      "Released":"2000-04-14",
+      "Runtime": 102,
+      "Genres": ["Crime", "Drama", "Horror"],
+      "Directors": ["Mary Harron"],
+      "Writers": ["Bret Easton Ellis", "Mary Harron", "Guinevere Turner"],
+      "Actors": ["Christian Bale", "Justin Theroux", "Josh Lucas"],
+      "Plot": "A wealthy New York City investment banking executive, Patrick Bateman, hides his alternate psychopathic ego from his co-workers and friends as he delves deeper into his violent, hedonistic fantasies.",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BNzBjM2I5ZjUtNmIzNy00OGNkLWIwZDMtOTAwYWUwMzA2YjdlXkEyXkFqcGc@._V1_SX300.jpg",
+      "Metascore": 64,
+      "imdbRating": 7.6
+    }
+  ]
+  res.send(movies);
 })
 
 app.listen(3000)
 
 console.log("Server now listening on http://localhost:3000/")
-
